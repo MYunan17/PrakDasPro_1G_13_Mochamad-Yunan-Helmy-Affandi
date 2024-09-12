6dfb08e1c058eb563185015e3e19159dc409c126
MYunan17 <hyunan08@gmail.com>
2024-09-12 21:03:50 +0700

    perkenalan diri

--- src/Pertemuan2/P2_Percobaan3_pertanyaan1_13.java
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_13.java
@@ -2,26 +2,16 @@ package Pertemuan2;
 
 public class P2_Percobaan3_pertanyaan1_13 {
     public static void main(String[] args) {
-        System.out.println("                  KARTU RENCANA STUDI") ;
-        System.out.println("NIM 244107020101") ;
-        System.out.println("NAMA : MOCHAMAD YUNAN HELMY AFFANDI");
-        System.out.println("PROGRAM STUDI : DIPLOMA IV Teknik Informatika");
-        System.out.println("KELAS : 1G");
-        System.out.println("-------------------------------------------------------------------");
-        System.out.println("| KODE MK | MATA KULIAH                             | SKS | JAM |");
-        System.out.println("-------------------------------------------------------------------");
-        System.out.println("|RT1241001|Pancasila                                  |2|    |2|");
-        System.out.println("|RT1241002|Konsep Teknologi Informasi                 |2|    |2|");
-        System.out.println("|RT1241003|Critical Thinking dan Problem Solving      |2|    |2|");
-        System.out.println("|RT1241004|Matematika Dasar                           |2|    |2|");
-        System.out.println("|RT1241005|Rekayasa Perangkta Lunak                   |2|    |2|");
-        System.out.println("|RT1241006|Dasar Pemrograman                          |2|    |2|");
-        System.out.println("|RT1241007|Praktikum Dasar Pemrograman                |2|    |2|");
-        System.out.println("|RT1241008|Keselamatan dan Kesehatan Kerja            |2|    |2|");
-        System.out.println("|------------------------------------------------------------------");
-        System.out.println("|         |           JUMLAH                          |18|   |14|");
-        System.out.println("-------------------------------------------------------------------");
+        System.out.println("----------------------------------------------");
+        System.out.println("--Data Mahasiswa Jurusan Teknologi Informasi--");
+        System.out.println("----------------------------------------------");
+        System.out.println("Nama     : Mochamad Yunan");
+        System.out.println("NIM      : 244107020101");
+        System.out.println("Kelas    : TI-1G");
+        System.out.println("No Absen : 13");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("No Hp    : 081617370962");
+        System.out.println("Hobi     : Berenang");
 
-        
     }
 }
