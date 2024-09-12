086d99734acb124621a6f13d99d1c3f985b99f36
MYunan17 <hyunan08@gmail.com>
2024-09-12 20:49:49 +0700

    ini krs saya

---
+++ src/Pertemuan2/P2_Percobaan3_pertanyaan1_13.java
@@ -0,0 +1,27 @@
+package Pertemuan2;
+
+public class P2_Percobaan3_pertanyaan1_13 {
+    public static void main(String[] args) {
+        System.out.println("                  KARTU RENCANA STUDI") ;
+        System.out.println("NIM 244107020101") ;
+        System.out.println("NAMA : MOCHAMAD YUNAN HELMY AFFANDI");
+        System.out.println("PROGRAM STUDI : DIPLOMA IV Teknik Informatika");
+        System.out.println("KELAS : 1G");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("| KODE MK | MATA KULIAH                             | SKS | JAM |");
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("|RT1241001|Pancasila                                  |2|    |2|");
+        System.out.println("|RT1241002|Konsep Teknologi Informasi                 |2|    |2|");
+        System.out.println("|RT1241003|Critical Thinking dan Problem Solving      |2|    |2|");
+        System.out.println("|RT1241004|Matematika Dasar                           |2|    |2|");
+        System.out.println("|RT1241005|Rekayasa Perangkta Lunak                   |2|    |2|");
+        System.out.println("|RT1241006|Dasar Pemrograman                          |2|    |2|");
+        System.out.println("|RT1241007|Praktikum Dasar Pemrograman                |2|    |2|");
+        System.out.println("|RT1241008|Keselamatan dan Kesehatan Kerja            |2|    |2|");
+        System.out.println("|------------------------------------------------------------------");
+        System.out.println("|         |           JUMLAH                          |18|   |14|");
+        System.out.println("-------------------------------------------------------------------");
+
+        
+    }
+}
