55ad150d3de907c66932e626c3808fbd1331b46e
MYunan17 <hyunan08@gmail.com>
2024-09-18 17:40:54 +0700

    P3_Percobaan1

---
+++ src/Pertemuan3/Siakad13.java
@@ -0,0 +1,31 @@
+package Pertemuan3;
+
+import java.util.Scanner;
+
+public class Siakad13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+    String nama, nim ;
+    char kelas ;
+    byte absen ;
+    Double nilaiKuis,nilaiTugas,nilaiUjian,nilaiAkhir;
+    System.out.print("Masukkan Nama :");
+    nama = sc.nextLine();
+    System.out.print("Masukkan NIM :");
+    nim = sc.nextLine();
+    System.out.print("Masukkan Kelas :");
+    kelas = sc.nextLine().charAt(1);
+    System.out.print("Masukkan nomor absen :");
+    absen = sc.nextByte();
+    System.out.print("Nilai kuis :");
+    nilaiKuis = sc.nextDouble();
+    System.out.print("Nilai Tugas :");
+    nilaiTugas = sc.nextDouble() ;
+    System.out.print("Nilai Ujian :");
+    nilaiUjian = sc.nextDouble() ;
+    nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian)/3 ;
+    System.out.println("Mahasiswa dengan Nama " + nama + " dan NIM :" + nim + " dari Kelas :" + kelas + " Absen :" + absen);
+    System.out.println("Nilai akhir :" + nilaiAkhir);
+
+    }
+}
