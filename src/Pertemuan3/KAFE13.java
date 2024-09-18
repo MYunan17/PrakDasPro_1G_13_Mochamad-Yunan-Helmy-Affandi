48407ff93620a51ed304d7b445bc7a7b53d1d60b
MYunan17 <hyunan08@gmail.com>
2024-09-19 01:53:58 +0700

    P3_Percobaan2_PertanyaanNo2

--- src/Pertemuan3/KAFE13.java
+++ src/Pertemuan3/KAFE13.java
@@ -8,7 +8,7 @@ public class KAFE13{
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        float diskon = 10 / 100f;
+        float diskon = 10 / 100;
         System.out.print("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
         System.out.print("Masukkan jumlah pembelian kopi: ");
