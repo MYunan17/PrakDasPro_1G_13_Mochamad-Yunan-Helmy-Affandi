5dd79238acba7ddca419102bfa833589c3be7d40
MYunan17 <hyunan08@gmail.com>
2024-10-03 06:11:28 +0700

    P5_Percobaan1_PertanyaanNo1

--- src/Pertemuan5/PemilihanBilangan13.java
+++ src/Pertemuan5/PemilihanBilangan13.java
@@ -5,15 +5,10 @@ public class PemilihanBilangan13 {
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
-        int angka ;
-        System.out.print("Massukan angka : ");
-        angka = sc.nextInt();
-        if (angka % 2  ==0 ) {
-            System.out.println("Angka " + angka + " terbilang bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " terbilang bilangan ganjil");
-        }
-    
+        int angka = 11 ;
+        String hasil ; 
+        hasil = (angka % 2 == 0) ? "Bilangan genap" : "Bilangan ganjil" ;
+        System.out.println(angka + " Termasuk " + hasil);
 
 }
 }
