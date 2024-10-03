15c859a48cda8ea9b2dbd187e8ee98c42632b0e0
MYunan17 <hyunan08@gmail.com>
2024-10-03 11:47:05 +0700

    P5_Percobaan2_PertanyaanNo6

---
+++ src/Pertemuan5/PemilihanOperator13.java
@@ -0,0 +1,39 @@
+package Pertemuan5;
+import java.util.Scanner ;
+
+public class PemilihanOperator13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+        double angka1, angka2,hasil= 0 ;
+        char operator ;
+
+        System.out.print("Massukan angka 1 : ");
+        angka1 = sc.nextDouble();
+        System.out.print("Massukan angka 2 : ");
+        angka2 = sc.nextDouble();
+        System.out.print("Massukan operator (+ - * /) : ");
+        operator = sc.next().charAt(0);
+        
+        if (operator=='+') {
+            hasil = angka1 + angka2 ;
+        } else {
+        }
+        if (operator=='-') {
+            hasil = angka1 - angka2 ;
+        } else {
+        }
+        if (operator=='*') {
+            hasil = angka1 * angka2 ;
+        } else {
+        }
+        if (operator=='/') {
+            hasil = angka1 / angka2 ;
+        } else {
+            System.out.println("operator yang dipilih tidak sesuai");
+        }
+    
+        System.out.println(angka1 + "" + operator + "" + angka2 + "=" + hasil);
+    }
+}
+    
