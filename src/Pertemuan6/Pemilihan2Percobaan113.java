fde7c592ccf5cb11555d025a9c00f2c2f5973154
MYunan17 <hyunan08@gmail.com>
2024-10-10 10:23:42 +0700

    P6_Percobaan1_pertanyaan4

--- src/Pertemuan6/Pemilihan2Percobaan113.java
+++ src/Pertemuan6/Pemilihan2Percobaan113.java
@@ -8,11 +8,13 @@ public class Pemilihan2Percobaan113 {
         int Tahun;
         System.out.print("Massukan Tahun : ");
         Tahun = input13.nextInt();
-       if ((Tahun%4) == 0 && (Tahun%100) !=0) {
-                System.out.println("Tahun Kabisat");  
-       } else {
-            System.out.println("Bukan Tahun Kabisat");
-            
-       }
+        if ((Tahun%4) == 0) {
+            System.out.println("Tahun kabisat") ;
+        } else if ((Tahun%100)== 0) {
+            System.out.println("Tahun kabisat") ;
+        } else if  ((Tahun%400)== 0) {
+            System.out.println("Tahun kabisat") ;
+        } else 
+        System.out.println("Bukan Tahun Kabisat");
     }
 }
