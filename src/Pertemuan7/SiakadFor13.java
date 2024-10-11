65bcbd35867848b7c94861905450b3ad88011a35
MYunan17 <hyunan08@gmail.com>
2024-10-11 14:36:02 +0700

    P7_Percobaan1_Pertanyaan5

--- src/Pertemuan7/SiakadFor13.java
+++ src/Pertemuan7/SiakadFor13.java
@@ -4,18 +4,27 @@ public class SiakadFor13 {
     
     public static void main (String[] args){
         Scanner sc = new Scanner (System.in) ;
-        double nilai,tertinggi=0, terendah = 100;
+        double nilai,tertinggi=0,terendah=100;
+        int Lulus=0, TidakLulus=0;
         for (int i =1; i <= 10; i++){
-            System.out.print("Massukan nilai mahasiswa ke-" + i + ":");
+            System.out.print("Massukan nilai mahasiswa ke-" + i + ": ");
             nilai =sc.nextDouble();
-            if (nilai > tertinggi) {
+            if (nilai>tertinggi) {
                 tertinggi = nilai;
             }
-            if (nilai < terendah) {
+            if (nilai<terendah) {
                 terendah = nilai;
+            }
+            if (nilai >= 60 ) {
+                Lulus ++;
+            }else{
+                TidakLulus++;
             }
         }
         System.out.println("Nilai tertinggi : " + tertinggi);
         System.out.println("Nilai terendah : " + terendah);
+        System.out.println("Jumlah Mahasiswa lulus : " + Lulus);
+        System.out.println("Jumlah Mahasiswa tidak lulus : " + TidakLulus);
+
     }
 }
