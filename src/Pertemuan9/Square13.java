5c2f68269973fba2e834d52fbfd7656af1bbcb22
MYunan17 <hyunan08@gmail.com>
2024-10-31 10:22:04 +0700

    P9_Percobaan2

---
+++ src/Pertemuan9/Square13.java
@@ -0,0 +1,17 @@
+package Pertemuan9;
+
+import java.util.Scanner;
+
+public class Square13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Massukan nilai N : ");
+        int N =sc.nextInt();
+        for (int iouter = 1; iouter <=N; iouter++) {
+            for (int i= 1; i <=N; i++) {
+                System.out.print("*");
+            }
+           System.out.println();
+        }
+    }
+}
