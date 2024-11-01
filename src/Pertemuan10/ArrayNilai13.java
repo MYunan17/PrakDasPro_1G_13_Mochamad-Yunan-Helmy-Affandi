c3ab5d64e8753b6062d2b37249aad61c1e44373e
MYunan17 <hyunan08@gmail.com>
2024-11-01 13:44:09 +0700

    P10_Percobaan2_Pertanyaan1

--- src/Pertemuan10/ArrayNilai13.java
+++ src/Pertemuan10/ArrayNilai13.java
@@ -6,7 +6,7 @@ public class ArrayNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         int [] nilaiakhir = new int [10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiakhir.length; i++) {
             System.out.print("Massukan nilai akhir ke-"+i+" : ");
             nilaiakhir[i] = sc.nextInt();
         }
