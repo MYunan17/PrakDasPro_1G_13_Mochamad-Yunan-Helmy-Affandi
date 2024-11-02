dec0076d285b889461bd67219c0f4d0532646d14
MYunan17 <hyunan08@gmail.com>
2024-11-02 12:25:21 +0700

    P10_Percobaan4_Pertanyaan3

--- src/Pertemuan10/SearchNilai13.java
+++ src/Pertemuan10/SearchNilai13.java
@@ -6,7 +6,7 @@ public class SearchNilai13 {
     public static void main(String[] args) {
         int[] arrNilai = new int[100];
         int key = 0;
-        int hasil = 0,jumlah;
+        int hasil = -1,jumlah;
         Scanner sc = new Scanner (System.in);
         System.out.print("Massukan banyaknya nilai yang akan di input : ");
         jumlah = sc.nextInt();
@@ -20,11 +20,17 @@ public class SearchNilai13 {
         for (int i = 0; i < jumlah; i++) {
             if (key == arrNilai[i]) {
                 hasil = i+1;
+                System.out.println();
+                System.out.println("Nilai " +key+" ditemukan , merupakan nilai Mahasiswa ke-"+hasil);
+                System.out.println();
                 break;
             }
+            
         }
-        System.out.println();
-        System.out.println("Nilai ditemukan" +key+" , merupakan nilai Mahasiswa ke-"+hasil);
-        System.out.println();
-    }
+      if (hasil == -1){
+       System.out.println();
+       System.out.println("Nilai yang dicari tidak ditemukan !!!");
+       System.out.println();
+     }
+   }
 }
