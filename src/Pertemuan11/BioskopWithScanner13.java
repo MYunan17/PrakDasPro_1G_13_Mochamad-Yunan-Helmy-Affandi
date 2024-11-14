e7e4d118ba99915580a366cc1d9ba28a2864170d
MYunan17 <hyunan08@gmail.com>
2024-11-14 10:34:18 +0700

    P11_Percobaan2

---
+++ src/Pertemuan11/BioskopWithScanner13.java
@@ -0,0 +1,30 @@
+package Pertemuan11;
+
+import java.util.Scanner;
+
+public class BioskopWithScanner13 {
+
+    public static void main(String[] args) {
+        String[] [] penonton = new String [4][2];
+        Scanner sc = new Scanner (System.in);
+        while (true) {
+            System.out.print("Massukan nama : ");
+            String nama = sc.nextLine();
+            System.out.print("Massukan baris : ");
+            int baris = sc.nextInt();
+            System.out.print("Massukan Kolom : ");
+            int Kolom = sc.nextInt();
+            sc.nextLine();
+
+            penonton[baris-1][Kolom-1] = nama ;
+
+            System.out.print("Input penonton lainnya? (y/n) : ");
+            String next = sc.nextLine();
+            if (next.equalsIgnoreCase("N")) {
+                break;
+            }
+            
+
+        }
+    }
+}
