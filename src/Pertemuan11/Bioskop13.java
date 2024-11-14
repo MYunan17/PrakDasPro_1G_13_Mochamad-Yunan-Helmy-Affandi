abe445479f27a6d8d7d2532deac58095b2c76bb5
MYunan17 <hyunan08@gmail.com>
2024-11-14 10:11:16 +0700

    P11_Percobaan1_Pertanyaan8

--- src/Pertemuan11/Bioskop13.java
+++ src/Pertemuan11/Bioskop13.java
@@ -13,9 +13,9 @@ public class Bioskop13 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
        System.out.println("Penonton pada baris ke-3 : ");
-       for (int i = 0; i < penonton[2].length; i++) {
-        System.out.println(penonton[2][i]);
-       }
+        for (String  i : penonton[2]) {
+            System.out.println(i);
+        }
 
     }
 }
