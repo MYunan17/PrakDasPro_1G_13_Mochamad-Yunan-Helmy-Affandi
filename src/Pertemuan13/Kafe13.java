2ad35571a1ee7f351b023d5710a30bcc4c551ba4
MYunan17 <hyunan08@gmail.com>
2024-11-29 13:59:08 +0700

    P13_Percobaan1

---
+++ src/Pertemuan13/Kafe13.java
@@ -0,0 +1,18 @@
+package Pertemuan13;
+
+public class Kafe13 {
+    public static void menu() {
+        System.out.println("========= MENU RESTO KAFE =========");
+        System.out.println("1. Kopi hitam  - RP 15.000");
+        System.out.println("2. Cappuccino - RP 20.000");
+        System.out.println("3. Latte - RP 22.000");
+        System.out.println("4. Teh Tarik - RP 12.000");
+        System.out.println("5. Roti Bakar - RP 10.000");
+        System.out.println("6.Mie Roti Goreng - RP 18.000");
+        System.out.println("==================================");
+        System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+    public static void main(String[] args) {
+        menu ();
+    }
+}
