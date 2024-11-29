4477e5f58d0ce93d2b585a7da80565460a546d9c
MYunan17 <hyunan08@gmail.com>
2024-11-29 14:54:11 +0700

    P13_Percobaan2_Pertanyaan5

--- src/Pertemuan13/Kafe13.java
+++ src/Pertemuan13/Kafe13.java
@@ -1,7 +1,19 @@
 package Pertemuan13;
 
 public class Kafe13 {
-    public static void main(String[] args) {
+    public static void menu(String namaPelangan, boolean ismember ,String diskon) {
+        System.out.println("Selamat datang, "+ namaPelangan + "!");
+        if (ismember) {
+            System.out.println("Anda adalah member,dapatkan diskon 10% untuk setiap pembelian");
+        }
+        System.out.println(" ");
+        if (diskon.equalsIgnoreCase("diskon50")) {
+            System.out.println("Kode promo digunakan !!!, anda mendapatkan diskon sebesar 50%");
+        }else if (diskon.equalsIgnoreCase("diskon30")) {
+            System.out.println("Kode promo digunakan !, anda mendapatkan diskon sebesar 30%");
+        }else System.out.println("kode invalid");
+
+
         System.out.println("========= MENU RESTO KAFE =========");
         System.out.println("1. Kopi hitam  - RP 15.000");
         System.out.println("2. Cappuccino - RP 20.000");
@@ -11,5 +23,10 @@ public class Kafe13 {
         System.out.println("6.Mie Roti Goreng - RP 18.000");
         System.out.println("==================================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
+    }
+    public static void main(String[] args) {
+        menu("Andi",true,"Diskon50");
+        menu("Dimas",true,"Diskon30");
+        menu("Dimas",true,"Diskon10");
     }
 }
