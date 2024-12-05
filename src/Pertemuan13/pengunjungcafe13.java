122a76b26437c700b359f23d2832353bd9bcd410
MYunan17 <hyunan08@gmail.com>
2024-12-05 13:49:49 +0700

    P13_Percobaan4_Pertanyaan2

--- src/Pertemuan13/pengunjungcafe13.java
+++ src/Pertemuan13/pengunjungcafe13.java
@@ -3,9 +3,9 @@ package Pertemuan13;
 public class pengunjungcafe13 {
     public static void daftarpengunjung(String... namapengunjung) {
        System.out.println("Daftar nama pengunjung : ");
-       for (int i = 0; i < namapengunjung.length; i++) {
-            System.out.println("- "+namapengunjung[i]);
-       } 
+       for (String nama : namapengunjung) {
+        System.out.println("- " + nama);
+    }
     }
     public static void main(String[] args) {
         daftarpengunjung("Ali","Budi","Citra");
