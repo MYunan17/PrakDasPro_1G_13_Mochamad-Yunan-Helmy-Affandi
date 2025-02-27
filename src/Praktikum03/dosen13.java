c5f1fb6dd17a3fffef520ca9cf6130a40fad6533
MYunan17 <hyunan08@gmail.com>
2025-02-27 13:35:43 +0700

    Pertemuan3_Tugas1

---
+++ src/Praktikum03/dosen13.java
@@ -0,0 +1,14 @@
+package Praktikum03;
+
+public class dosen13 {
+    String kode;
+    String nama;
+    Boolean jeniskelamin;
+    int usia;
+    public dosen13(String kode,String nama,Boolean jeniskelamin,int usia){
+        this.kode = kode;
+        this.nama = nama;
+        this.jeniskelamin = jeniskelamin;
+        this.usia = usia;
+    }
+}
